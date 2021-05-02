0936a1e4c0c7bf79171ae337bba0cd63d3c24084
Erikomoreira <erik.oliveira.moreira@hotmail.com>
2021-05-02 18:58:17 -0300

    Exemplo Azure

---
+++ spring-boot-jwt-azure/src/test/java/br/com/erik/azure/springbootjwtazure/SpringBootJwtAzureApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.erik.azure.springbootjwtazure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtAzureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
